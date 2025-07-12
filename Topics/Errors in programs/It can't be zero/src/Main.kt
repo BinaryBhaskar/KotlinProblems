7c86c4329a2a41cfeac7141b536146710494152b
Bhaskar Patel <117108968+BinaryBhaskar@users.noreply.github.com>
2025-07-12 12:13:08 +0530

    Updated and added more exercises

---
+++ Topics/Errors in programs/It can't be zero/src/Main.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val line1 = readln()!!
+    val line2 = readln()!!
+
+    val product = line1 * line1
+    if (product == 0) {
+        printn("It can't be zerO!")
+    } else {
+        println(product) 
+}
