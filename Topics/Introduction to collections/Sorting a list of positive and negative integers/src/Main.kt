2fa00f7d862a0efe740e9130168b71433d883075
Bhaskar Patel <117108968+BinaryBhaskar@users.noreply.github.com>
2025-07-13 16:41:07 +0530

    Removed //TODO

--- Topics/Introduction to collections/Sorting a list of positive and negative integers/src/Main.kt
+++ Topics/Introduction to collections/Sorting a list of positive and negative integers/src/Main.kt
@@ -7,15 +7,12 @@ fun main(args: Array<String>) {
     // Use a scanner to read the user input
     val scanner = Scanner(System.`in`)
 
-    // TODO: Extract the integers from the input and add them to the mutable list
     repeat(5) {
         val num = scanner.nextInt()
         numberList.add(num)
     }
 
-    // TODO: Sort the list in ascending order
     numberList.sort()
 
-    // TODO: Iterate through the sorted list and print each number followed by a space.
     for (i in numberList) print("$i ")
 }
